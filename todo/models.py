ab3711a0954d7f1e29b448594cf6c0781e87ccd7
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-08-04 22:52:13 +0900

    modified:   todo/models.py         new file:   todo/templates/edit.html         modified:   todo/templates/index.html         modified:   todo/views.py

--- todo/models.py
+++ todo/models.py
@@ -15,6 +15,7 @@ class User(UserMixin, db.Model):
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean, default=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     user = db.relationship("User", backref=db.backref("todos", lazy=True))
--- todo/views.py
+++ todo/views.py
@@ -10,7 +10,11 @@ bp = Blueprint("views", __name__)
 @bp.route("/")
 def index():
     if current_user.is_authenticated:
-        todos = Todo.query.filter_by(user_id=current_user.id).all()
+        todos = (
+            Todo.query.filter_by(user_id=current_user.id)
+            .order_by(db.asc(Todo.complete))
+            .all()
+        )
         return render_template("index.html", todos=todos)
     return render_template("index.html")
 
@@ -19,7 +23,7 @@ def index():
 @login_required
 def add():
     todo_text = request.form.get("todo")
-    new_todo = Todo(text=todo_text, user_id=current_user.id)
+    new_todo = Todo(text=todo_text, user_id=current_user.id)  # type: ignore
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("views.index"))
@@ -28,10 +32,38 @@ def add():
 @bp.route("/delete/<int:todo_id>")
 @login_required
 def delete(todo_id):
-    todo_to_delete = Todo.query.get_or_404(todo_id)
-    if todo_to_delete.user_id != current_user.id:
-        flash("You don't have permission to delete this item.", "danger")
-        return redirect(url_for("views.index"))
+    todo_to_delete = Todo.query.filter_by(
+        id=todo_id, user_id=current_user.id
+    ).first_or_404()
     db.session.delete(todo_to_delete)
     db.session.commit()
     return redirect(url_for("views.index"))
+
+
+@bp.route("/complete/<int:todo_id>", methods=["POST"])
+@login_required
+def complete(todo_id):
+    todo_to_complete = Todo.query.filter_by(
+        id=todo_id, user_id=current_user.id
+    ).first_or_404()
+    todo_to_complete.complete = not todo_to_complete.complete
+    db.session.commit()
+    flash("To-do item status updated.", "success")
+    return redirect(url_for("views.index"))
+
+
+@bp.route("/edit/<int:todo_id>", methods=["GET", "POST"])
+@login_required
+def edit(todo_id):
+    todo_to_edit = Todo.query.filter_by(
+        id=todo_id, user_id=current_user.id
+    ).first_or_404()
+    if request.method == "POST":
+        new_text = request.form.get("todo")
+        if new_text:
+            todo_to_edit.text = new_text
+            db.session.commit()
+            flash("To-do item updated successfully.", "success")
+        return redirect(url_for("views.index"))
+
+    return render_template("edit.html", todo=todo_to_edit)
